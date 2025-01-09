4f85989eb4c87897316c978d57cfba8bbe7b1330
hwangshinae <dasokot@naver.com>
2025-01-09 17:35:17 +0900

    Update stringart.js

--- stringart.js
+++ stringart.js
@@ -32,7 +32,7 @@ function degreeToRad(degree) {
 function draw() {
   background(255, 255, 255);
   translate(width / 2, height / 2);
-  let radi = 250; //원의 반지름
+  let radi = 150; //원의 반지름
   
   circle(0, 0, radi * 2);
 
